71eea7c3d6a2a7330afd31b9694a092025968f28
Duncan Parker <zflhflasyyajbkx@protonmail.com>
2025-06-16 19:26:20 -0400

    chore: update build config paths

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  root: 'src',
+  root: 'frontend',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'frontend'),
     },
   },
 });
